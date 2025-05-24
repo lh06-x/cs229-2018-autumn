af687f8c06194e1096e5dbcfd39571ee6f639f2a
令狐文韬 <lhwtxsw0514@163.com>
2025-05-24 22:53:22 +0800

    ps01_b

--- problem-sets/PS1/src/p01b_logreg.py
+++ problem-sets/PS1/src/p01b_logreg.py
@@ -2,6 +2,7 @@ import numpy as np
 import util
 
 from linear_model import LinearModel
+from notes.backprop import sigmoid
 
 
 def main(train_path, eval_path, pred_path):
@@ -15,6 +16,11 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    clf = LogisticRegression()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_eval)
+    np.savetxt(pred_path, y_pred > 0.5, fmt='%d')
     # *** END CODE HERE ***
 
 
@@ -26,6 +32,26 @@ class LogisticRegression(LinearModel):
         > clf.fit(x_train, y_train)
         > clf.predict(x_eval)
     """
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def compute_cost(self, x, y):
+        h = self.sigmoid(x.dot(self.theta))
+        m = x.shape[0]
+        cost = - (1 / m) * (y.dot(np.log(h)) + (1 - y).dot(np.log(1 - h)))
+        return cost
+
+    def compute_gradient(self, x, y):
+        h = self.sigmoid(x.dot(self.theta))
+        m = x.shape[0]
+        grad = (1 / m) * (x.T.dot(h - y))
+        return grad
+
+    def compute_hessian(self, x, y):
+        h = self.sigmoid(x.dot(self.theta))
+        m = x.shape[0]
+        hessian = (1 / m) * (x.T * h * (1 - h).dot(x))
+        return hessian
 
     def fit(self, x, y):
         """Run Newton's Method to minimize J(theta) for logistic regression.
@@ -35,6 +61,23 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+
+        n_features = x.shape[1]
+        self.theta = np.random.normal(0, 0.01, n_features)
+
+        for i in range(self.max_iter):
+            cost = self.compute_cost(x, y)
+            grad = self.compute_gradient(x, y)
+            hessian = self.compute_hessian(x, y)
+            new_theta = np.linalg.solve(hessian, grad)
+            self.theta = self.theta - new_theta
+            if np.linalg.norm(new_theta - self.theta) < self.eps:
+                print(f"Converged after {i + 1} iterations")
+                break
+
+        self.theta = new_theta
+        return self
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -47,4 +90,6 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        prob = self.sigmoid(x.dot(self.theta))
+        return (prob >= 0.5).astype(int)
         # *** END CODE HERE ***
